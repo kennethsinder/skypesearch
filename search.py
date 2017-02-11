fa0a7d386dd77907200a2f97b5dc22286790b3ae
Kenneth Sinder <kennethsinder@gmail.com>
2017-02-10 19:47:23 -0500

    feat(folderstructure): Created main script file

---
+++ search.py
@@ -0,0 +1,16 @@
+#################################
+# Programmer: Kenneth Sinder
+# Date Created: 2017-02-10
+# Filename: search.py
+# Description: SQLite3 DB Search Script
+#################################
+
+import argparse
+import os
+import sys
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
